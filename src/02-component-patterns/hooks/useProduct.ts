9bbd98fd099f35341dab9b7b37641d7b58f57f4c
Moises Barillas <moisesrocha717@gmail.com>
2022-03-28 12:02:43 -0600

    control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -9,13 +9,8 @@ interface UseProductArgs {
 
 export const useProduct = ({ onChange, product, value = 0 }: UseProductArgs) => {
 	const [counter, setCounter] = useState(value);
-	const isControlled = useRef(!!onChange);
 
 	const increaseBy = (val: number) => {
-		if (isControlled.current) {
-			// signo de admiración onChange! significa que estas segur@ que la variable onChange existe
-			return onChange!({ product, quantity: val });
-		}
 		let newValue = Math.max(counter + val, 0);
 		setCounter(newValue);
 
--- src/02-component-patterns/hooks/useShopping.ts
+++ src/02-component-patterns/hooks/useShopping.ts
@@ -6,33 +6,18 @@ const useShopping = () => {
 
 	const handleProductChange = ({ quantity, product }: { quantity: number; product: Product }) => {
 		setShoppingCart((oldShoppingCart) => {
-			// refactor
-			const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, quantity: 0 };
-
-			if (Math.max(productInCart.quantity + quantity, 0) > 0) {
-				productInCart.quantity += quantity;
-				return {
-					...oldShoppingCart,
-					[product.id]: productInCart,
-				};
+			///Primer caso:
+			if (quantity === 0) {
+				const { [product.id]: _, ...newShoppingCart } = oldShoppingCart;
+				return newShoppingCart;
 			}
-
-			// Borrar el producto
-			const { [product.id]: _, ...newShoppingCart } = oldShoppingCart;
-			return newShoppingCart;
-
-			// Primer caso:
-			// if (quantity === 0) {
-			//     const { [product.id]: _, ...newShoppingCart } = oldShoppingCart;
-			//     return newShoppingCart;
-			// }
-			// return {
-			//     ...oldShoppingCart,
-			//     [product.id]: {
-			//         ...product,
-			//         quantity
-			//     }
-			// }
+			return {
+				...oldShoppingCart,
+				[product.id]: {
+					...product,
+					quantity,
+				},
+			};
 		});
 	};
 
